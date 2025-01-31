7b1a3bac4ff72f909ca2bfff649551c620667979
SpookyJelly <zang9412@gmail.com>
2025-02-01 00:14:19 +0900

    gh page deploy init

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,8 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+import type { StorybookConfig } from "@storybook/react-vite";
+import "sb-addon-permutation-table";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
---
+++ src/stories/Apple.stories.ts
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "applejelly";
+import { ICON_POSITION, iconKeys, LEVEL, TSHIRT_SIZE } from "../constants";
+
+const meta = {
+  title: "Example/Apple",
+  component: Button,
+  args: {
+    label: "Button",
+  },
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: { control: "select", options: TSHIRT_SIZE },
+    href: { control: "text" },
+    isExternal: { control: "boolean" },
+    level: {
+      control: "select",
+      options: LEVEL,
+    },
+    icon: { control: "select", options: iconKeys },
+    iconPosition: { control: "select", options: ICON_POSITION },
+    isBorderless: { control: "boolean" },
+    isDangerouslyNaked: { control: "boolean" },
+    isNaked: { control: "boolean" },
+    isShade: { control: "boolean" },
+    isFullWidth: { control: "boolean" },
+    isActive: { control: "boolean" },
+    isHovered: { control: "boolean" },
+    isLoading: { control: "boolean" },
+    isDisabled: { control: "boolean" },
+    isPrimary: { control: "boolean" },
+    label: { control: "text" },
+    featureStatus: { control: "text" },
+    type: { control: "select", options: ["button", "submit", "reset"] },
+    hasEllipsis: { control: "boolean" },
+    ariaAttrs: { control: "object" },
+    dataAttrs: { control: "object" },
+    ariaLabel: { control: "text" },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {},
+};
+
+export const Primary: Story = {
+  args: {
+    isPrimary: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    isDisabled: true,
+  },
+};
